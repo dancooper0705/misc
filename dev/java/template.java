4062144c04fcbd9cf428ecbd5020b9f70262a859
dancooper <dancooper0705@gmail.com>
2019-08-29 17:03:35 +0800

    dev: add dev files

---
+++ dev/java/template.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+import java.util.regex.*;
+import java.awt.geom.*;
+
+class Main
+{
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+    }
+
+}
---
+++ dev/python/b.py
@@ -0,0 +1,11 @@
+import sys
+import math
+import re
+import io
+
+def main():
+    s = input().lower()
+    print(s)
+
+if __name__ == "__main__":
+    main()
