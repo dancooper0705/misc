6ad9f0f1f4f590aaf22338a77936a4c159cc7920
dancooper <dancooper0705@gmail.com>
2019-08-29 18:16:25 +0800

    dev: add leetcode tmplate file

---
+++ dev/cpp/leetcode_template.cpp
@@ -0,0 +1,49 @@
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<vii> vvii;
+
+template<typename S, typename T>
+ostream& operator<<(ostream& out, const pair<S, T> p) {
+    out << "(" << p.first << "," << p.second << ")";
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (auto a: v)
+        out << a << " ";
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const set<T>& S) {
+    for (auto a: S)
+        cout << a << " ";
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const multiset<T>& S) {
+    for (auto a: S)
+        cout << a << " ";
+    return out;
+}
+
+template<typename S, typename T>
+ostream& operator<<(ostream& out, const map<S, T>& M) {
+    for (auto m: M)
+        cout << "(" << m.first << "->" << m.second << ") ";
+    return out;
+}
+
+template<typename S, typename T>
+pair<S, T> operator+(pair<S, T> a, pair<S, T> b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+template<typename S, typename T>
+pair<S, T> operator-(pair<S, T> a, pair<S, T> b) {
+    return make_pair(a.first - b.first, a.second - b.second);
+}
