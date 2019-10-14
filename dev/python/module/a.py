812171c3e26f661c7156198fd3ec4de980253352
dancooper <dancooper0705@gmail.com>
2019-10-14 19:41:14 +0800

    dcstat: add sample.mean_confidence_level()

--- dev/python/module/a.py
+++ dev/python/module/a.py
@@ -3,10 +3,12 @@ import dcstat
 
 def main():
     sample = dcstat.sample()
-    for i in range(10):
+    for i in range(30):
         sample.add_element(i)
     print(sample.mean())
     print(sample.variance())
+    print(sample.deviation())
+    print(sample.mean_confidence_interval(90))
 
 if __name__ == "__main__":
     main()
--- dev/python/module/dcstat.py
+++ dev/python/module/dcstat.py
@@ -1,3 +1,5 @@
+import math
+
 class sample:
     arr = []
 
@@ -14,6 +16,9 @@ class sample:
         avg = total / len(self.arr)
         return avg
 
+    def count(self):
+        return len(self.arr)
+
     def variance(self):
         ans = 0
         x = 0
@@ -24,3 +29,27 @@ class sample:
             x2 = x2 + a ** 2
         ans = (x2 - x **2 / n) / (n - 1)
         return ans
+
+    def deviation(self):
+        return math.sqrt(self.variance())
+
+    def mean_confidence_interval(self, confidence_level):
+        mean = self.mean)
+        deviation = self.deviation() / math.sqrt(self.count())
+        z_critial_value = 0
+        if confidence_level == 80:
+            alpha = 0.1
+            z_critical_value = 1.28
+        elif confidence_level == 90:
+            alpha = 0.05
+            z_critical_value = 1.645
+        elif confidence_level == 95:
+            alpha = 0.025
+            z_critical_value = 1.96
+        elif confidence_level == 98:
+            alpha = 0.001
+            z_critical_value = 2.33
+        elif confidence_level == 99:
+            alpha = 0.0005
+            z_critical_value = 2.58
+        return [mean - z_critical_value * deviation, mean + z_critical_value * deviation]
