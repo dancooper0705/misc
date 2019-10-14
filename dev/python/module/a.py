7d28083c566858f4d25df7dec4696d6efedb9629
dancooper <dancooper0705@gmail.com>
2019-10-14 17:30:24 +0800

    module: add module directory

---
+++ dev/python/module/a.py
@@ -0,0 +1,9 @@
+import sys
+import dcstat
+
+def main():
+    arr = [1, 3, 4, 5]
+    print(dcstat.mean(arr))
+
+if __name__ == "__main__":
+    main()
---
+++ dev/python/module/dcstat.py
@@ -0,0 +1,6 @@
+def mean(arr):
+    total = 0
+    for a in arr:
+        total += a
+    avg = total / len(arr)
+    return avg
