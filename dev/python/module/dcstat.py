4f6997c767cad9ffadb0ed52a3a04605a0859428
dancooper <dancooper0705@gmail.com>
2019-10-15 17:20:30 +0800

    dcstat: add confidence_interval_of_population_proportion()

--- dev/python/module/dcstat.py
+++ dev/python/module/dcstat.py
@@ -1,6 +1,15 @@
 import math
 import scipy.stats
 
+def confidence_interval_of_population_proportion(sample_size, sample_proportion, confidence_level):
+    mean = sample_proportion
+    standard_deviation = math.sqrt(sample_proportion * (1 - sample_proportion) / sample_size)
+    confidence_coefficient = confidence_level / 100
+    alpha_level = 1 - confidence_coefficient
+    tail_area = alpha_level / 2
+    z_score = scipy.stats.norm.ppf(1 - tail_area)
+    return [mean - z_score * standard_deviation, mean + z_score * standard_deviation]
+
 def confidence_interval_of_population_mean_with_normal_distribution(sample_size, sample_mean, sample_standard_deviation, confidence_level):
     mean = sample_mean
     standard_deviation = sample_standard_deviation / math.sqrt(sample_size)
@@ -91,6 +100,14 @@ def main():
     print('90% confidence interval for population mean: ' + ' to '.join('{0:.2f}'.format(a) for a in confidence_interval_of_population_mean(sample_size, sample_mean, sample_standard_deviation, 90)))
     print('95% confidence interval for population mean: ' + ' to '.join('{0:.2f}'.format(a) for a in confidence_interval_of_population_mean(sample_size, sample_mean, sample_standard_deviation, 95)))
     print('99% confidence interval for population mean: ' + ' to '.join('{0:.2f}'.format(a) for a in confidence_interval_of_population_mean(sample_size, sample_mean, sample_standard_deviation, 99)))
+
+    print('example3, p = sample proportion, n*p >= 5 && n*(1-p)>=5')
+    sample_size = 800
+    sample_proportion = 0.7
+    print('sample_size: ' + str(sample_size))
+    print('sample_proportion: ' + str(sample_proportion))
+    print('90% confidence interval for population proportion: ' + ' to '.join('{0:.4f}'.format(a) for a in confidence_interval_of_population_proportion(sample_size, sample_proportion, 90)))
+    print('95% confidence interval for population proportion: ' + ' to '.join('{0:.4f}'.format(a) for a in confidence_interval_of_population_proportion(sample_size, sample_proportion, 95)))
 
 if __name__ == "__main__":
     main()
