42d8a5c1ff6ad67614f9da15d5de1f10c0155ada
dancooper <dancooper0705@gmail.com>
2019-10-17 14:25:28 +0800

    dcstats modify variable name

--- dev/python/module/dcstats.py
+++ dev/python/module/dcstats.py
@@ -57,9 +57,9 @@ def two_tailed_critical_z_score(significance):
     critical_z_score = scipy.stats.norm.ppf(1 - tail_area)
     return critical_z_score
 
-def test_statistics_score(hypothesis_mean, sample_size, sample_mean, sample_standard_deviation):
+def test_statistics_score(population_mean, sample_size, sample_mean, sample_standard_deviation):
     standard_deviation = sample_standard_deviation / math.sqrt(sample_size)
-    z_score = (sample_mean - hypothesis_mean) / standard_deviation
+    z_score = (sample_mean - population_mean) / standard_deviation
     return z_score
 
 def upper_tailed_p_value_with_z_score(z_score):
