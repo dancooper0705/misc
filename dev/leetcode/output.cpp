ccec5a79af4e9b21ec7af8a54834d459d2d5beba
dancooper <dancooper0705@gmail.com>
2019-10-20 10:25:40 +0800

    dev: leetcode: add output functions

---
+++ dev/leetcode/output.cpp
@@ -0,0 +1,43 @@
+template<typename S, typename T>
+ostream& operator<<(ostream& out, const pair<S, T> p) {
+    out << "(" << p.first << "," << p.second << ")";
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const vector<T>& v) {
+    for (auto a: v)
+        out << a << " ";
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const set<T>& S) {
+    for (auto a: S)
+        cout << a << " ";
+    return out;
+}
+
+template<typename T>
+ostream& operator<<(ostream& out, const multiset<T>& S) {
+    for (auto a: S)
+        cout << a << " ";
+    return out;
+}
+
+template<typename S, typename T>
+ostream& operator<<(ostream& out, const map<S, T>& M) {
+    for (auto m: M)
+        cout << "(" << m.first << "->" << m.second << ") ";
+    return out;
+}
+
+template<typename S, typename T>
+pair<S, T> operator+(pair<S, T> a, pair<S, T> b) {
+    return make_pair(a.first + b.first, a.second + b.second);
+}
+
+template<typename S, typename T>
+pair<S, T> operator-(pair<S, T> a, pair<S, T> b) {
+    return make_pair(a.first - b.first, a.second - b.second);
+}
